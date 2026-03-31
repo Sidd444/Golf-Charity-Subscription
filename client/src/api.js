18b14b1009bd4c417d752517e922f12a93d234ce
Siddhartha Bharali <siddharthabharaliassam@gmail.com>
2026-03-31 20:25:04 +0530

    server url connected

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,5 @@
-const server_url='http://localhost:5000';
+//const server_url='http://localhost:5000';
+
+const server_url='https://golf-charity-subscription.onrender.com';
 
 export default server_url;
